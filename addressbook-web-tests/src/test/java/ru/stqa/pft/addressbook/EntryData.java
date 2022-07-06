fc50c6069f40362d6e0480df8940271f536c3fd7
LHS-666 <108588058+LHS-666@users.noreply.github.com>
2022-07-06 19:02:54 +0400

    EntryDataTest

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/EntryData.java
@@ -0,0 +1,4 @@
+package ru.stqa.pft.addressbook;
+
+public record EntryData(String firstname, String lastname, String address, String phone) {
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/NewEntryTest.java
@@ -0,0 +1,119 @@
+package ru.stqa.pft.addressbook;
+
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class NewEntryTest {
+  private WebDriver wd;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  private JavascriptExecutor js;
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "src/test/chromedriver.exe");
+    wd = new ChromeDriver();
+    wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    js = (JavascriptExecutor) wd;
+    wd.get("http://localhost/addressbook/edit.php");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+  }
+
+  @Test
+  public void testUntitledTestCase() throws Exception {
+
+    gotoNewEntry();
+    EntryData(new EntryData("Vasya", "Pupkin", "Red Square, Moscow", "911"));
+    submitNewEntry();
+    gotoHomePage();
+    Logout();
+  }
+
+  private void Logout() {
+    wd.get("http://localhost/addressbook/index.php");
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void gotoHomePage() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+
+  private void submitNewEntry() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  private void EntryData(EntryData entryData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(entryData.firstname());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(entryData.lastname());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(entryData.address());
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(entryData.phone());
+  }
+
+  private void gotoNewEntry() {
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wd.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = wd.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}

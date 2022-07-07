1474d2576056d32ebb15ed3571f43fb88e241aad
LHS-666 <108588058+LHS-666@users.noreply.github.com>
2022-07-07 21:55:41 +0400

    Делегирование. Часть 1

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,24 +1,21 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.time.Duration;
 
-public class TestBase {
-    protected WebDriver wd;
+public class ApplicationManager {
+    public WebDriver wd;
     private JavascriptExecutor js;
 
-    @BeforeMethod(alwaysRun = true)
-    public void setUp() throws Exception {
-      System.setProperty("webdriver.chrome.driver", "src/test/chromedriver.exe");
-      wd = new ChromeDriver();
-      wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-      js = (JavascriptExecutor) wd;
-      wd.get("http://localhost/addressbook/");
-      login("admin", "secret");
+    public void init() {
+        wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        js = (JavascriptExecutor) wd;
+        wd.get("http://localhost/addressbook/");
+        login("admin", "secret");
     }
 
     private void login(String username, String password) {
@@ -31,15 +28,15 @@ public class TestBase {
       wd.findElement(By.xpath("//input[@value='Login']")).click();
     }
 
-    protected void returnToGroupPafe() {
+    public void returnToGroupPafe() {
       wd.findElement(By.linkText("group page")).click();
     }
 
-    protected void submitGroupCreation() {
+    public void submitGroupCreation() {
       wd.findElement(By.name("submit")).click();
     }
 
-    protected void fillGroupForm(GroupData groupData) {
+    public void fillGroupForm(GroupData groupData) {
       wd.findElement(By.name("group_name")).click();
       wd.findElement(By.name("group_name")).clear();
       wd.findElement(By.name("group_name")).sendKeys(groupData.name());
@@ -51,18 +48,16 @@ public class TestBase {
       wd.findElement(By.name("group_footer")).sendKeys(groupData.footer());
     }
 
-    protected void initGroupCreation() {
+    public void initGroupCreation() {
       wd.findElement(By.name("new")).click();
     }
 
-    protected void gotoGroupPage() {
+    public void gotoGroupPage() {
       wd.findElement(By.linkText("groups")).click();
     }
 
-    @AfterMethod(alwaysRun = true)
-    public void tearDown() throws Exception {
-      wd.quit();
-
+    public void stop() {
+        wd.quit();
     }
 
     private boolean isElementPresent(By by) {
@@ -83,11 +78,11 @@ public class TestBase {
       }
     }
 
-    protected void deleteSelectedGroup() {
+    public void deleteSelectedGroup() {
       wd.findElement(By.name("delete")).click();
     }
 
-    protected void selectGroup() {
+    public void selectGroup() {
       wd.findElement(By.name("selected[]")).click();
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletingTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletingTests.java
@@ -1,4 +1,4 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.*;
 
@@ -6,10 +6,10 @@ public class GroupDeletingTests extends TestBase {
 
   @Test
   public void testGroupDeleting() throws Exception {
-    gotoGroupPage();
-    selectGroup();
-    deleteSelectedGroup();
-    returnToGroupPafe();
+    app.gotoGroupPage();
+    app.selectGroup();
+    app.deleteSelectedGroup();
+    app.returnToGroupPafe();
   }
 
 }

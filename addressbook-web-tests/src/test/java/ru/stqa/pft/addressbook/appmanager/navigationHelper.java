2aec8eee7abaf4c78eaabcb545837c8cb6f28e9d
LHS-666 <108588058+LHS-666@users.noreply.github.com>
2022-07-14 09:15:41 +0400

    оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/navigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/navigationHelper.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 public class navigationHelper extends HelperBase {
@@ -11,7 +12,29 @@ public class navigationHelper extends HelperBase {
     }
 
     public void gotoGroupPage() {
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+
+        }
 
         click(By.linkText("groups"));
+    }
+
+    private boolean isElementPresent(By locator) {
+        try {
+            wd.findElement(locator);
+            return true;
+        } catch (NoSuchElementException ex) {
+            return false;
+        }
+    }
+
+    public void gotoHomePage () {
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
+        click(By.linkText("Home"));
     }
 }
